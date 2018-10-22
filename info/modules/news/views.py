3c6fd5d8681242a6d14025a3eb5d1ab24c5c6bb1
zqk <2209847031@qq.com>
2018-10-22 11:53:46 +0800

    新闻详情页  详情页热门新闻展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,3 +1,8 @@
+from flask import abort, jsonify
+from flask import current_app
+
+from info.models import News
+from info.utils.response_code import RET
 from . import news_blue
 from flask import render_template
 
@@ -8,6 +13,45 @@ from flask import render_template
 # 返回值: detail.html页面, 用户data字典数据
 @news_blue.route('/<int:news_id>')
 def news(news_id):
-    return render_template("news/detail.html")
+    """
+    # 思路分析
+    # 1.根据传入的新闻编号,获取新闻对象
+    # 2.判断新闻是否存在
+    # 3.查询热门新闻数据
+    # 4.将新闻列表转成,字典列表
+    # 5.携带数据渲染页面
+    """
+    # 1 根据传入的新闻编号,获取新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg = "新闻获取失败")
+
+
+    # 2 判断新闻是否存在
+    if not news:
+        abort (404)
+
+    # 3 查询热门新闻数据
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(8).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="查询数据失败")
+
+
+    # 4 将新闻列表转成,字典列表
+    click_news_list = []
+    for item in news_list:
+        click_news_list.append(item.to_dict())
+
+    data = {
+        "news":news.to_dict(),
+        "click_news_list":click_news_list
+    }
+
+    # 5 携带数据渲染页面
+    return render_template("news/detail.html",data = data)
 
 
