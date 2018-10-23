40f59772f6d72b0193bf152f440541c953a151e3
zqk <2209847031@qq.com>
2018-10-24 00:49:02 +0800

    个人基本资料显示

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,8 +1,20 @@
 from flask import g
+from flask import redirect
 
 from info.utils.common import user_login_data
 from . import user_blue
 from flask import render_template
+
+# 功能描述: 获取基本资料
+# 请求路径: /user/base_info
+# 请求方式:GET,POST
+# 请求参数:POST请求有参数,nick_name,signature,gender
+# 返回值:errno,errmsg
+@user_blue.route('/base_info', methods=['GET', 'POST'])
+@user_login_data
+def base_info():
+
+    return render_template("news/user_base_info.html",user=g.user.to_dict())
 
 #展示个人中心页面
 # 请求路径: /user/info
@@ -12,6 +24,9 @@ from flask import render_template
 @user_blue.route('/info')
 @user_login_data
 def user_index():
+
+    if not g.user:
+        return redirect("/")
     data = {
         "user_info":g.user.to_dict() if g.user else ""
     }
