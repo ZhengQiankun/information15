93400554629b26e22fcd1dde893168fcef99f91c
zqk <2209847031@qq.com>
2018-10-23 23:42:18 +0800

    用户模板继承，右上角用户登录状态显示

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -1,3 +1,6 @@
+from flask import g
+
+from info.utils.common import user_login_data
 from . import user_blue
 from flask import render_template
 
@@ -7,5 +10,9 @@ from flask import render_template
 # 请求参数:无
 # 返回值: user.html页面,用户字典data数据
 @user_blue.route('/info')
+@user_login_data
 def user_index():
-    return render_template("news/user.html")
+    data = {
+        "user_info":g.user.to_dict() if g.user else ""
+    }
+    return render_template("news/user.html",data = data)
